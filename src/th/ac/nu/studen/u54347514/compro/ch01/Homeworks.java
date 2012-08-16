69ed0db0f55f798451427e099b54ac7e2a86e45f
Jirapat Prasong <bas_zero_one@hotmail.com>
2012-08-16 22:14:21 +0700

    chapter 5

--- src/th/ac/nu/studen/u54347514/compro/ch01/Homeworks.java
+++ src/th/ac/nu/studen/u54347514/compro/ch01/Homeworks.java
@@ -1,19 +1,26 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package th.ac.nu.studen.u54347514.compro.ch01;
-
-/**
- *
- * @author Acer
- */
+import java.util.Scanner;
 public class Homeworks {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-}
+public static void main(String[] args) {
+  double x;
+    double a;
+     a = 3.14;
+     
+   System.out.println("เลือกการคำนวณ");
+    System.out.println("1.พื้นที่ผิว 2. ปริมาตรทรงกลม");
+    Scanner s = new Scanner(System.in);
+       x = s.nextInt();
+        if(x<=1){
+       System.out.println("หาพื้นที่ผิว");
+       x = s.nextDouble();
+       System.out.println(4*a*(x*x));
+       }
+        else{
+            if(x>=2)
+       System.out.println("หาปริมาตรทรงกลม");
+       x = s.nextDouble();
+       System.out.println(((x*x*x)*a*4/3));
+        }
+        }
+      }
---
+++ src/th/ac/nu/studen/u54347514/compro/ch01/Homeworkss.java
@@ -0,0 +1,16 @@
+package th.ac.nu.studen.u54347514.compro.ch01;
+public class Homeworkss {
+
+    public static void main(String[] args) {
+       
+int x = 20;
+int y = 25;
+System.out.println("sum : " + (x + y ));
+System.out.println("Difference : " + (x-y));
+System.out.println("Product : " + (x*y));
+System.out.println("Average : " + ((x+y)/2.0));
+System.out.println("Distacne : " + Math.abs(x-y));
+System.out.println("Maximum :" + Math.max(x,y));
+System.out.println("Minimum :"+ Math.min(x,y));
+    }
+}
